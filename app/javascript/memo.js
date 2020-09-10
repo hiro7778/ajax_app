2fd1932c7c0bac89ccc31dc25adc67bc83ab064a
hiro7778 <stfhm10969@gmail.com>
2020-09-10 17:49:56 +0900

    メモ投稿機能のサーバーサイド実装

---
+++ app/javascript/memo.js
@@ -0,0 +1,7 @@
+function memo() {
+  const submit = document.getElementById("submit");
+  submit.addEventListener("click", (e) => {
+    const XHR = new XMLHttpRequest();
+  });
+}
+window.addEventListener("load", memo);
